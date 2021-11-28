af4e5228173e7d86a93de9ad9c8a242dc559475d
alexsandrbarabash <sbarabas176@gmail.com>
2021-11-29 00:03:37 +0200

    change method session

--- src/session/session.controller.ts
+++ src/session/session.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './get-user.decorator';
 
 @Controller('session')
 export class SessionController {
   @UseGuards(AuthGuard('jwt'))
-  @Post()
+  @Get()
   checkSession(@GetUser() user): any {
     return user;
   }
